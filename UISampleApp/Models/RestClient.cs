5dc6300d48a7aa453733a83357aa48cb6e3b3b0b
CHAVEZ RIVAS <jese.chavez@ulatina.net>
2020-01-19 20:39:36 -0600

    Autentificacion JWT

---
+++ UISampleApp/Models/RestClient.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UISampleApp.Models
+{
+    public class RestClient
+    {
+        HttpClient client;
+        //Get consumer
+        public async Task<T> GetT<T>(string url)
+        {
+            client = new HttpClient();
+            try
+            {
+
+
+                var response = await client.GetAsync(url);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    //Peticion correcta
+                    var jsonStringResponse = await response.Content.ReadAsStringAsync();
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(jsonStringResponse);
+                }
+                else
+                    return default(T);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<string> PostToken(string url, Object content, Type ez)
+        {
+            client = new HttpClient();
+            HttpResponseMessage response = null;
+
+            try
+            {
+                var json = JsonConvert.SerializeObject(Convert.ChangeType(content, ez));
+                var contentString = new StringContent(json, Encoding.UTF8, "application/json");
+
+                response = await client.PostAsync(url, contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var stringRespond = await response.Content.ReadAsStringAsync();
+                    if (stringRespond != string.Empty)
+                        return stringRespond;
+                    else
+                        return string.Empty;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return string.Empty;
+        }
+
+        public async Task<string> PostTokenBody(string url,Object content,Type ez)
+        {
+            client = new HttpClient();
+            HttpResponseMessage response = null;
+
+            try
+            {
+                var json = JsonConvert.SerializeObject(Convert.ChangeType(content, ez));
+                var contentString = new StringContent(json, Encoding.UTF8, "application/json");
+
+                response = await client.PostAsync(url, contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var stringRespond = await response.Content.ReadAsStringAsync();
+                    var jwtDynamic = JsonConvert.DeserializeObject<dynamic>(stringRespond);
+                    var accessToken = jwtDynamic.Value<string>("access_token");
+                    return accessToken;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return string.Empty;
+        }
+    }
+}
--- UISampleApp/Views/Logins/PaginaGeraldLogin.xaml.cs
+++ UISampleApp/Views/Logins/PaginaGeraldLogin.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UISampleApp.Models;
 using UISampleApp.Views.Home;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,6 +13,8 @@ namespace UISampleApp.Views.Logins
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PaginaGeraldLogin : ContentPage
 	{
+        RestClient proc = new RestClient();
+
 		public PaginaGeraldLogin ()
 		{
 			InitializeComponent ();
@@ -19,7 +22,33 @@ namespace UISampleApp.Views.Logins
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            await Xamarin.Forms.Application.Current.MainPage.Navigation.PushModalAsync(new RootHomePage());
+            if (txtPassword.Text != string.Empty && txtUser.Text != string.Empty)
+            {
+                proc = new RestClient();
+                var response = await proc.PostToken("https://eecommerapi.conveyor.cloud/api/login/authenticateclient", new LoginClientRequest()
+                {
+                    Password = txtPassword.Text
+                ,
+                    User = txtUser.Text
+                }, typeof(LoginClientRequest));
+
+                //                var responseJason = await proc.PostTokenBody("https://eecommerapi.conveyor.cloud/api/login/authenticateclient", new LoginClientRequest()
+                //                {
+                //                    Password = txtPassword.Text
+                //,
+                //                    User = txtUser.Text
+                //                }, typeof(LoginClientRequest));
+
+
+                if (response!=string.Empty)
+                    await Xamarin.Forms.Application.Current.MainPage.Navigation.PushModalAsync(new RootHomePage());
+                else
+                    await DisplayAlert("Error!", "Credenciales incorrectas", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Error!", "Todos los datos son obligatorios", "OK");
+            }
         }
 
         protected override bool OnBackButtonPressed()
